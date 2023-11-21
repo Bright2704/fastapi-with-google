31c4504689f414b22ca451763eb9048920b06faf
Bright2704 <thanonchaikantha27@gmail.com>
2023-11-22 00:57:14 +0700

    first commit

--- app/config.py
+++ app/config.py
@@ -6,3 +6,4 @@ load_dotenv()
 
 CLIENT_ID = os.environ.get('client-id', None)
 CLIENT_SECRET = os.environ.get('client-secret', None)
+CLIENT_REDIRECT_URI = os.environ.get('client-redirect-uri', None)
--- app/main.py
+++ app/main.py
@@ -1,94 +1,98 @@
-from fastapi import FastAPI
-from fastapi.templating import Jinja2Templates
-from starlette.requests import Request
-from starlette.responses import RedirectResponse
-from starlette.middleware.sessions import SessionMiddleware
-from authlib.integrations.starlette_client import OAuth, OAuthError
-from .config import CLIENT_ID, CLIENT_SECRET
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Depends , Request 
+from fastapi.security import OAuth2PasswordBearer
+import requests
+from jose import jwt
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from app.config import CLIENT_ID, CLIENT_REDIRECT_URI, CLIENT_SECRET
+load_dotenv()
+
+
+
+
+# Initialize MongoDB client
+mongo_client = MongoClient('localhost', 27017 )  # Update with your MongoDB connection URL
+db = mongo_client['mongo-db']  # Update with your database name
+users_collection = db['users']  # Collection to store user records
+
+# Define the function to create a user record in the database
+def create_user_in_database(user_info):
+    user_record = {
+        'email': user_info['email'],
+        'name': user_info['name'],
+        'picture': user_info['picture'],
+    
+    }
+    users_collection.insert_one(user_record)
+
 
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="add any string...")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-oauth = OAuth()
-oauth.register(
-    name='google',
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    client_kwargs={
-        'scope': 'email openid profile',
-        'redirect_url': 'http://localhost:8000/auth'
+# Replace these with your own values from the Google Developer Console
+GOOGLE_CLIENT_ID = CLIENT_ID
+GOOGLE_CLIENT_SECRET = CLIENT_SECRET
+GOOGLE_REDIRECT_URI = CLIENT_REDIRECT_URI
+
+
+
+@app.get("/login/google")
+async def login_google():
+    return {
+        "message": "Login with Google",
+        "url": f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline"
     }
-)
+
+@app.get("/auth")
+async def auth_google(code: str):
+    token_url = "https://accounts.google.com/o/oauth2/token"
+    data = {
+        "code": code,
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uri": GOOGLE_REDIRECT_URI,
+        "grant_type": "authorization_code",
+    }
+    response = requests.post(token_url, data=data)
+    access_token = response.json().get("access_token")
+    if response.status_code != 200:
+        return {"error": "Failed to fetch user information from Google"}
+    
+    user_info_response = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
+    
+    if user_info_response.status_code == 200:
+        user_info = user_info_response.json()
+        
+        # Check if the user already exists in the database based on their email
+        existing_user = users_collection.find_one({'email': user_info['email']})
+        
+        if not existing_user:
+            # Create a new user record in your database with the user's information
+            create_user_in_database(user_info)
+        
+        return user_info  # You can return the user's information if needed
+    else:
+        # Handle the case when the user information request fails
+        return {"error": "Failed to fetch user information from Google"}
+
+   
+
+@app.get("/token")
+async def get_token(token: str = Depends(oauth2_scheme)):
+    return jwt.decode(token, GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
 
 
-templates = Jinja2Templates(directory="templates")
-
+    
+@app.get("/logout")
+async def logout(request: Request):
+    request.session.clear()
+    return {"message": "Successfully logged out"}
 
 @app.get("/")
-def index(request: Request):
-    user = request.session.get('user')
-    if user:
-        return RedirectResponse('welcome')
-
-    return templates.TemplateResponse(
-        name="home.html",
-        context={"request": request}
-    )
+async def root():
+    return {"message": "Hello World"}
 
 
-@app.get('/welcome')
-def welcome(request: Request):
-    user = request.session.get('user')
-    if not user:
-        return RedirectResponse('/')
-    return templates.TemplateResponse(
-        name='welcome.html',
-        context={'request': request, 'user': user}
-    )
-
-
-@app.get("/login")
-async def login(request: Request):
-    url = request.url_for('auth')
-    return await oauth.google.authorize_redirect(request, url)
-
-
-@app.get('/auth')
-async def auth(request: Request):
-    try:
-        token = await oauth.google.authorize_access_token(request)
-    except OAuthError as e:
-        return templates.TemplateResponse(
-            name='error.html',
-            context={'request': request, 'error': e.error}
-        )
-    user = token.get('userinfo')
-    if user:
-        request.session['user'] = dict(user)
-        print("Access Token:", token)
-        return RedirectResponse('/welcome')
-    
-    return RedirectResponse('/')
-
-# @app.get('/auth')
-# async def auth(request: Request):
-#     try:
-#         token = await oauth.google.authorize_access_token(request)
-#     except OAuthError as e:
-#         return templates.TemplateResponse(
-#             name='error.html',
-#             context={'request': request, 'error': e.error}
-#         )
-#     user = token.get('userinfo')
-#     if user:
-#         request.session['user'] = dict(user)
-#     return RedirectResponse('welcome')
-
-
-@app.get('/logout')
-def logout(request: Request):
-    request.session.pop('user')
-    return RedirectResponse('/')
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
